3b1f008409515d349f9f9b12e9db579a9b142b42
Luiz Fernando <lufmalta@gmail.com>
2018-09-22 13:37:02 -0300

    Projeto - Curriculo-site(versao 1)

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+$(function(){
+	var a = $('#a').html();
+	a = 'Experiência - '+$('#a').attr('href');
+	$('#a').html(a);
+});
---
+++ index.php
@@ -0,0 +1,207 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Developed by Luiz Fernando Malta Martins</title>
+	<meta charset="UTF-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+	<link 	rel="stylesheet" type="text/css" href="assets/css/bootstrap4-css/bootstrap.min.css"/>
+	<style type="text/css">
+		section {
+			position:absolute;
+			width:140px;
+			height:1080px;
+			background-color:rgba(0, 0, 0, 0.5);
+		}
+		section .row,
+		.habilidades  {
+			padding:20px 10px;
+		}
+		h6 {
+			border-bottom:1px solid #CCC;
+			padding-bottom:7px;
+		}
+		section .col,
+		.habilidades h6
+		 {
+			color:#FFF;
+		}
+		ul{
+			text-align:left;
+		}
+		ul li {
+			color:#FFF;
+			font-size:13.5px;
+		}
+		.hab-conteudo {
+			margin-right:10px;
+		}
+		.col3{
+			background-color:#CCC;
+		}
+	</style>
+
+
+</head>
+<body >
+		<section >
+			<div class="row" >
+				<div class="col" >
+					<h6 style="">Informação de Contato</h6>
+				</div>
+				<div class="col" style="font-size:12px;">
+					E-mail<br/>
+					lufmalta@gmail.com
+				</div>
+
+				<div class="col" style="font-size:12px;">
+					<p></p>
+					Endereço<br/>
+					Rua u-53<br/>
+					Quadra 15<br/>
+					lote 10, casa 2<br/>
+					Setor Vila União
+				</div>
+				<div class="col" style="font-size:12px;">
+					<p></p>
+					Telefone<br/>
+					+55 62 982396838
+				</div>
+			</div>
+			<div class="habilidades">
+				<div class="hab-conteudo ">
+					<h6 style="">Habilidades</h6>
+					<ul >
+						<li>Desenvolvedor web - Junior</li>
+						<li>PHP - Orientação à Objetos</li>
+						<li>HTML - HTML5</li>
+						<li>CSS - CSS3</li>
+						<li>Bootstrap 3</li>
+						<li>Bootstrap 4</li>
+						<li>jQuery</li>
+						<li>Javascript</li>
+						<li>Git - Versionamento de código</li>
+						<li>Banco de dados(MySql)</li>
+					</ul>
+				</div>
+				
+			</div>
+			
+		</section>
+	<header style="background-color:#CCC;">
+		<div class="container" style='margin:45px;'>
+			<div style="margin-left:80px;border-bottom:2px solid #DDD;">
+				<h2 style="text-align:left;word-wrap:wrap;text-align:center;">Luiz Fernando Malta Martins</h2><br/>
+				<h4 style="text-align:left;font-size:20px;">Desenvolvedor web front-end, desenvolvedor web back-end. Procuro me ingressar no
+				mercado de trabalho em alguma empresa de desenvolvimento de software, e/ou, que possua
+				desenvolvedores de software.</h4>	
+			</div>			
+		</div>		
+	</header>
+
+	<main style="background-color:#DDD;">
+
+		<div class="container" style="margin-top:20px;background-color: #DDD;margin:40px;">
+			<h5 style="margin-left:80px;text-align:center;">Experiência</h5><br/>
+			<div style="margin-left:80px;border-bottom:2px solid #DDD;background-color:#DDD;">
+				
+				<div class="ConteudoExp" style="margin-left:80px;border-bottom:2px solid #DDD;">
+					<div class="row">
+						<div class="col-sm-3 col3" >
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Charlotte Casa de Dança</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Goiânia, GO</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Junho - 2018</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Agosto - 2018</h6>
+						</div>
+						<div class="col-sm-9" style="background-color:#CCC;">
+							<h4><img src="assets/images/star.png" style="padding-right:10px;"/>Diretor Executivo</h4>
+							<span>Compra de produtos para venda no dia do evento, seja alimento, bebidas, gelo, materiais de limpeza, copos descartáveis. Reserva de mesas para evento, organizar a limpeza do galpão. No dia do evento, ser o responsável pelo caixa de entradas e fichas.</span>
+						</div>						
+					</div>
+					<br/>					
+					<div class="row">
+						<div class="col-sm-3 col3" >
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Edíficio Dj.Oliveira</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Goiânia, GO</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Maio - 2018</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Julho - 2018</h6>
+						</div>
+						<div class="col-sm-9" style="background-color:#CCC;">
+							<h4><img src="assets/images/star.png" style="padding-right:10px;"/>Porteiro Noturno</h4>
+							<span>Organização da guarita, fechamento dos portões da garagem, abertura do portão de entrada para
+							moradores, documentação de rotina diária, recebimento de correspondências e entrega
+							das mesmas. Vigilância de câmeras, entrega de recados e/ou utensílios de moradores.</span>
+						</div>						
+					</div>
+					<br/>
+					<div class="row">
+						<div class="col-sm-3 col3" >
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Hinode</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Goiânia, GO</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Maio - 2016</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Julho - 2018</h6>
+						</div>
+						<div class="col-sm-9" style="background-color:#CCC;">
+							<h4><img src="assets/images/star.png" style="padding-right:10px;"/>Consultor Executivo</h4>
+							<span>Vendas diretas de produtos, seja cosméticos, perfumes, shampoos, cremes corporais e faciais.</span>
+						</div>						
+					</div>
+					<br/>
+					<div class="row">
+						<div class="col-sm-3 col3" >
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Universidade Salgado de Oliveira</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Goiânia, GO</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Maio - 2014</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Maio - 2016</h6>
+						</div>
+						<div class="col-sm-9" style="background-color:#CCC;">
+							<h4><img src="assets/images/star.png" style="padding-right:10px;"/>Assistente de Laboratório</h4>
+							<span>Manutenção de laboratórios de informática, formatação de computadores, atendimento à alunos na matricula, no início e fim de semestre.</span>
+						</div>						
+					</div>
+
+				</div>
+			</div>
+		</div>
+		
+	</main>
+	<footer style="background-color:#DDD;">
+		<div class="container" style="margin-top:20px;background-color: #CCC;margin:40px;">
+			<h5 style="margin-left:80px;text-align:center;">Educação</h5><br/>
+			<div style="margin-left:80px;">
+				<div class="ConteudoExp" style="margin-left:80px;border-bottom:1px solid #DDD;">
+
+					<div class="row">
+						<div class="col-sm-3 col3" >
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Universidade Salgado de Oliveira</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">Setor Sul, Goiânia</h6>
+							<h6 style="text-align:center;border-bottom:1px solid #DDD;">2016</h6>							
+						</div>
+						<div class="col-sm-9" style="background-color:#CCC;">
+							
+							<h4><img src="assets/images/star.png" style="padding-right:10px;"/>Analise e Desenvolvimento de Sistemas</h4>
+							<span>Curso PHP - Zero ao profissional - Professor Bonieky Lacerda(Sênior PHP) - (Cursando). Certificados já adquiridos: Banco de dados(MySql) - 10 horas, Bootstrap Básico - 10 horas, Fundamentos PHP - 20 horas, HTML5 e CSS3 - 20 horas, Javascript - 25 horas. Módulos concluídos: 14 módulos, total de 405 aulas concluídas. </span>
+							<a id="a" href="https://github.com/lufmalta/projetoteste2" target="_blank"></a>
+						</div>						
+					</div>					
+					<br/>	
+
+				</div>
+			</div>
+		</div>
+	</footer>
+	
+	
+
+
+
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+
+	<script type="text/javascript" src="assets/js/bootstrap4-js/bootstrap.bundle.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+
+
+
+	
+</body>
+</html>
